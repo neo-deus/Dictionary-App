bc00d0c5a7b5aaeb70cb8597381eb13332f521a4
neo_deus <arpandesp7890@gmail.com>
2023-04-17 18:23:22 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,174 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _url = 'https://owlbot.info/api/v4/dictionary/';
+  String _token = 'd166297c41adc84ea6c604d22b221238c25f0ba4';
+
+  TextEditingController _inputWord = TextEditingController();
+
+  StreamController _streamController = StreamController();
+  late Stream _stream;
+
+  @override
+  void initState() {
+    //_streamController = StreamController();
+    _stream = _streamController.stream;
+    super.initState();
+  }
+
+  _search() async {
+    if (_inputWord.text == null || _inputWord.text.length == 0) {
+      _streamController.add(null);
+      return;
+    }
+    _streamController.add("waiting");
+    /*try {*/
+    Response response = await get(Uri.parse(_url + _inputWord.text.trim()),
+        headers: {"Authorization": "Token " + _token});
+    if (response.statusCode == 200) {
+      _streamController.add(json.decode(response.body));
+    } else {
+      _streamController.add("wrong");
+    }
+    /*} on FormatException catch (e) {
+      if (e.message.contains('404')) {
+        _streamController.add("wrong");
+      } else {
+        _streamController.add("wrong");
+      }
+    } catch (e) {
+      _streamController.add("wrong");
+    }*/
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dictionary'),
+        backgroundColor: Colors.black54,
+        foregroundColor: Colors.white,
+        centerTitle: true,
+        bottom: PreferredSize(
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(
+                    left: 12,
+                    bottom: 8,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  child: TextFormField(
+                    onChanged: (String word) {},
+                    controller: _inputWord,
+                    decoration: InputDecoration(
+                      hintText: 'Search for a word',
+                      contentPadding: EdgeInsets.only(left: 24),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: _search,
+                icon: Icon(
+                  Icons.search,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          preferredSize: Size.fromHeight(48),
+        ),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(8),
+        child: StreamBuilder(
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            if (snapshot.data == null) {
+              return Center(
+                child: Text('Enter a word to search'),
+              );
+            }
+            if (snapshot.data == 'waiting') {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            if (snapshot.data == 'wrong') {
+              return Center(
+                child: Text('Invalid search - Please enter a valid word'),
+              );
+            }
+
+            return ListView.builder(
+              itemBuilder: ((context, index) {
+                return ListBody(
+                  children: [
+                    Container(
+                      color: Colors.grey[300],
+                      child: ListTile(
+                        leading: snapshot.data["definitions"][index]
+                                    ["image_url"] ==
+                                null
+                            ? null
+                            : CircleAvatar(
+                                backgroundImage: NetworkImage(snapshot
+                                    .data["definitions"][index]["image_url"]),
+                              ),
+                        title: Text(_inputWord.text.trim() +
+                            "(" +
+                            snapshot.data["definitions"][index]["type"] +
+                            ")"),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                          snapshot.data["definitions"][index]["definition"]),
+                    )
+                  ],
+                );
+              }),
+              itemCount: snapshot.data["definitions"].length,
+            );
+          },
+          stream: _stream,
+        ),
+      ),
+    );
+  }
+}
